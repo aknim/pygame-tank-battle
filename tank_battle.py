9bfe62911c561c784d9cd50e5c24b29d535e2ea4
akshaya <akshayasunny@gmail.com>
2024-11-16 14:19:30 +0530

    Day1.2 20x1 Added enemies moving randomly

--- tank_battle.py
+++ tank_battle.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -8,8 +9,12 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 TANK_WIDTH = 40
 TANK_HEIGHT = 40
+
 TANK_COLOR = (0, 255, 0) # Green
 BLACK = (0, 0, 0)
+ENEMY_COLOR = (255, 0, 0) # Red
+
+ENEMY_COUNT = 5
 
 # Create the game screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -38,11 +43,50 @@ class PlayerTank:
     def draw(self):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
+class EnemyTank:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = TANK_WIDTH
+        self.height = TANK_HEIGHT
+        self.color = ENEMY_COLOR
+        self.speed = 3 # slower than the player
+
+    def move_randomly(self):
+        direction = random.choice(['up', 'down', 'left', 'right'])
+        if direction == 'up' and self.y > 0:
+            self.y -= self.speed
+        elif direction == 'down' and self.y < SCREEN_HEIGHT - self.height:
+            self.y += self.speed
+        elif direction == 'left' and self.x > 0:
+            self.x -= self.speed
+        elif direction == 'right' and self.x < SCREEN_WIDTH - self.width:
+            self.x += self.speed
+
+    def move_towards_player(self, player_x, player_y):
+        if self.x < player_x:
+            self.x += self.speed
+        elif self.x > player_x:
+            self.x -= self.speed
+
+        if self.y < player_y:
+            self.y += self.speed
+        elif self.y > player_y:
+            self.y -= self.speed
+        
+    def draw(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        
 
 # Main game loop
 def main():
     clock = pygame.time.Clock()
     player_tank = PlayerTank(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+    enemy_tanks = [EnemyTank(random.randint(0, SCREEN_WIDTH - TANK_WIDTH), 
+                            random.randint(0, SCREEN_HEIGHT - TANK_HEIGHT)) 
+                            for _ in range(ENEMY_COUNT)]
+
 
     while True:
         screen.fill(BLACK)
@@ -55,6 +99,12 @@ def main():
         # Check for player input
         keys = pygame.key.get_pressed()
         player_tank.move(keys)
+
+        # Move and draw each enemy tank
+        for enemy in enemy_tanks:
+            enemy.move_randomly()
+            # enemy.move_towards_player(player_tank.x, player_tank.y)
+            enemy.draw()
 
         # Draw player tank
         player_tank.draw()
